761af853d111ea20bd0733fde2f6aecefec7f867
Rahul <rkc.98@outlook.com>
2021-09-01 11:35:00 +0530

    added navigation support

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './navigation/AppNavigator';
 import OnBoarding from './screens/OnBoarding';
 import ThirdDemo from './screens/ThirdDemo';
 import TwoDemo from './screens/TwoDemo';
@@ -12,7 +13,8 @@ export default function App() {
     <>
     {/* <OnBoarding /> */}
   {/* <TwoDemo /> */}
-  <ThirdDemo />
+  {/* <ThirdDemo /> */}
+  <AppNavigator />
   </>
     )
 }
--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import OnBoarding from '../screens/OnBoarding'
+import ThirdDemo from '../screens/ThirdDemo'
+import TwoDemo from '../screens/TwoDemo'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={OnBoarding} />
+                <Stack.Screen name="second" component={TwoDemo} />
+                <Stack.Screen name="third" component={ThirdDemo} />
+                
+
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
 
